0bcb743e989c9a6a2142091db68b6854704df56d
Bruce Ellis <bruce.ellis@gmail.com>
2014-02-28 04:13:17 +1100

    Command error returns

--- rose/cmd.go
+++ rose/cmd.go
@@ -5,7 +5,7 @@ import (
 )
 
 type req struct {
-	cmd	func(int, []string, string, int, bool) []string
+	cmd	func(int, []string, string, int, bool) ([]string, int)
 	min	int
 	max	int
 	usage	string
@@ -23,21 +23,26 @@ var (
 	}
 )
 
-func Run_cmd(args []string) []string {
+func Run_cmd(args []string) ([]string, int) {
 	return run_cmdx(len(args), args, "", 0, false)
 }
 
-func Run_cmds(vect [][]string, src string, die bool) (ret [][]string) {
+func Run_cmds(vect [][]string, src string, die bool) (ret [][]string, errc int, errv []int) {
 	ret = make([][]string, 0)
+	errc = 0
+	errv = make([]int, 0)
 	for i, args := range vect {
-		r := run_cmdx(len(args), args, src, i + 1, die)
+		r, e := run_cmdx(len(args), args, src, i + 1, die)
 		ret = append(ret, r)
+		errc += e
+		errv = append(errv, e)
 	}
-	return ret
+	return
 }
 
-func run_cmdx(argc int, args []string, src string, ix int, die bool) (ret []string) {
+func run_cmdx(argc int, args []string, src string, ix int, die bool) (ret []string, err int) {
 	ret = none
+	err = 0
 	if argc == 0 {
 		return
 	}
@@ -54,6 +59,7 @@ func run_cmdx(argc int, args []string, src string, ix int, die bool) (ret []stri
 			fatal(src, ix, mesg)
 		} else {
 			fmt.Println(mesg)
+			err = 1
 			return
 		}
 	}
@@ -62,11 +68,11 @@ func run_cmdx(argc int, args []string, src string, ix int, die bool) (ret []stri
 
 // Commands
 
-func cmd_comment(argc int, args []string, src string, ix int, die bool) []string {
-	return nil
+func cmd_comment(argc int, args []string, src string, ix int, die bool) ([]string, int) {
+	return nil, 0
 }
 
-func cmd_echo(argc int, args []string, src string, ix int, die bool) []string {
+func cmd_echo(argc int, args []string, src string, ix int, die bool) ([]string, int) {
 	e := "";
 	for i, s := range args {
 		if i == 0 {
@@ -80,14 +86,14 @@ func cmd_echo(argc int, args []string, src string, ix int, die bool) []string {
 	}
 	r := make([]string , 1, 1)
 	r[0] = e
-	return r
+	return r, 0
 }
 
-func cmd_help(argc int, args []string, src string, ix int, die bool) []string {
+func cmd_help(argc int, args []string, src string, ix int, die bool) ([]string, int) {
 	fmt.Println("I need somebody!")
-	return none
+	return none, 0
 }
 
-func cmd_root(argc int, args []string, src string, ix int, die bool) []string {
-	return none
+func cmd_root(argc int, args []string, src string, ix int, die bool) ([]string, int) {
+	return none, 0
 }
