eec40b989491b91c2f2a0259d9bbb59b05562387
brucee <bruce.ellis@gmail.com>
2014-09-18 20:40:41 +1000

    Update

--- shapefile/quad.go
+++ shapefile/quad.go
@@ -1,37 +1,54 @@
 package shapefile
 
-type quad struct {
+type Quad struct {
 	box  bbox
 	qbox []*bbox
-	down []*quad
+	down []*Quad
 	only *subreg
 	full []*Region
 }
 
 type subreg struct {
-	box bbox
-	reg *Region
+	depth int
+	box   bbox
+	reg   *Region
 }
 
-func MakeQuad(b *bbox) *quad {
-	q := new(quad)
+func MakeQuad(b *bbox) *Quad {
+	q := new(Quad)
 	q.box = *b
 	return q
 }
 
-func (q *quad) AddRegion(r *Region) {
+func (q *Quad) AddRegion(r *Region) {
 	s := new(subreg)
 	s.box = r.poly.bounds
 	s.reg = r
 	q.addsubreg(s)
 }
 
-func (q *quad) addsubreg(s *subreg) {
+func (q *Quad) addsubreg(s *subreg) {
+	if q.down == nil {
+		if q.only == nil {
+			q.only = s
+			return
+		}
+		q.populate()
+		q.addsubreg(s)
+		q.only = nil
+	}
+	if q.box.equal(&s.box) {
+		if q.full == nil {
+			q.full = make([]*Region, 0)
+		}
+		q.full = append(q.full, s.reg)
+		return
+	}
 }
 
-func (q *quad) populate() {
+func (q *Quad) populate() {
 	q.qbox = q.box.divide()
-	q.down = make([]*quad, 4, 4)
+	q.down = make([]*Quad, 4, 4)
 	for i := 0; i < 4; i++ {
 		q.down[i] = MakeQuad(q.qbox[i])
 	}
