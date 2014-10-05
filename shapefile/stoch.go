4ab5d49d3a36f76db98cc73b1860b5ed827ffa87
brucee <bruce.ellis@gmail.com>
2014-10-05 16:43:38 +1100

    Update

--- shapefile/stoch.go
+++ shapefile/stoch.go
@@ -2,6 +2,11 @@ package shapefile
 
 import (
 	"fmt"
+	"math/rand"
+)
+
+const (
+	epsz = 4
 )
 
 type axdesc struct {
@@ -11,8 +16,26 @@ type axdesc struct {
 
 func (s *Shapefile) StochEps(n int) {
 	fmt.Println("StochEps:")
+	xd := mkAxdesc(s.box.xmin, s.box.xmax)
+	yd := mkAxdesc(s.box.ymin, s.box.ymax)
+	r := rand.New(rand.NewSource(666))
+	e := make([]int, epsz+1, epsz+1)
+	for i := 0; i < n; i++ {
+		x := xd.choose(r)
+		y := yd.choose(r)
+		c := s.quad.SearchEps(&point{x: x, y: y})
+		if c < epsz {
+			e[c]++
+		} else {
+			e[epsz]++
+		}
+	}
 }
 
 func mkAxdesc(min, max float64) *axdesc {
 	return &axdesc{v0: min, vz: max - min}
+}
+
+func (a *axdesc) choose(r *rand.Rand) float64 {
+	return a.v0 + r.Float64()*a.vz
 }
