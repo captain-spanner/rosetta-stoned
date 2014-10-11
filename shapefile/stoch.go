1aa4968ef624d81ac6a557c7410aa9bcb388af5d
brucee <bruce.ellis@gmail.com>
2014-10-11 18:45:31 +1100

    Update

--- shapefile/stoch.go
+++ shapefile/stoch.go
@@ -14,20 +14,50 @@ type axdesc struct {
 	vz float64
 }
 
+func (s *Shapefile) Stoch(n int, seed int64) {
+	fmt.Printf("Stoch: count %d, seed %d\n", n, seed)
+	xd := mkAxdesc(s.box.xmin, s.box.xmax)
+	yd := mkAxdesc(s.box.ymin, s.box.ymax)
+	r := rand.New(rand.NewSource(seed))
+	sz := len(s.regs)
+	e := make([]int, sz+1, sz+1)
+	for i := 0; i < n; i++ {
+		x := xd.choose(r)
+		y := yd.choose(r)
+		c := s.quad.Search(&point{x: x, y: y})
+		if c < 0 {
+			e[sz]++
+		} else {
+			e[c]++
+		}
+	}
+	for i, v := range e {
+		if v == 0 {
+			continue
+		}
+		if i == sz {
+			fmt.Printf("Pirate land: %d (%d%%)\n", v, v*100/n)
+		} else {
+			fmt.Printf("Reg %d: %d\n", i, v)
+		}
+	}
+}
+
 func (s *Shapefile) StochEps(n int, seed int64) {
 	fmt.Printf("StochEps: count %d, seed %d\n", n, seed)
 	xd := mkAxdesc(s.box.xmin, s.box.xmax)
 	yd := mkAxdesc(s.box.ymin, s.box.ymax)
 	r := rand.New(rand.NewSource(seed))
-	e := make([]int, epsz+1, epsz+1)
+	sz := epsz
+	e := make([]int, sz+1, sz+1)
 	for i := 0; i < n; i++ {
 		x := xd.choose(r)
 		y := yd.choose(r)
 		c := s.quad.SearchEps(&point{x: x, y: y})
-		if c < epsz {
+		if c < sz {
 			e[c]++
 		} else {
-			e[epsz]++
+			e[sz]++
 		}
 	}
 	for i, v := range e {
@@ -39,7 +69,7 @@ func (s *Shapefile) StochEps(n int, seed int64) {
 			fmt.Printf("Pirate land: %d (%d%%)\n", v, v*100/n)
 		case 1:
 			fmt.Printf("Claimed: %d\n", v)
-		case epsz:
+		case sz:
 			fmt.Printf("Nutty: %d\n", v)
 		default:
 			fmt.Printf("Multi %d: %d\n", i, v)
