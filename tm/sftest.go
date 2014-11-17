1c76ec9edd5cf9c3da95fa45a2fcd807280f34fa
brucee <bruce.ellis@gmail.com>
2014-11-17 17:33:21 +1100

    pprof

--- tm/sftest.go
+++ tm/sftest.go
@@ -3,19 +3,36 @@ package main
 import (
 	"fmt"
 	"shapefile"
+
+	"flag"
+	"log"
+	"os"
+	"runtime/pprof"
 )
 
 const (
-	path	string	= "/home/brucee/TM/TM_WORLD_BORDERS-0.3"
-	test	bool	= true
+	// path	string	= "/home/brucee/TM/TM_WORLD_BORDERS-0.3"
+	path string = "/n/fossil/index/TM/TM_WORLD_BORDERS-0.3"
+	test bool   = true
 )
 
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+
 func main() {
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
 	s, err := shapefile.MakeShapefile(path, nil)
 	if err != nil {
 		fmt.Printf("%s: %s\n", path, err.Error())
 	}
 	if test {
-		s.Stoch(10000000, 1127)
+		s.Stoch(10000000, 11271)
 	}
 }
